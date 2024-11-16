f8a4f2497c29e77748d4fb17f02662c343a04706
sourav <ramsourav82@gmail.com>
2024-11-16 09:33:16 +0600

    web view youtube

---
+++ app/src/main/java/com/example/webview/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.webview
+
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import com.example.webview.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.webview.loadUrl("https://www.youtube.com/")
+
+        binding.webview.settings.javaScriptEnabled = true
+
+
+        binding.webview.webViewClient = object : WebViewClient(){
+
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+
+                binding.webview.visibility =View.GONE
+
+                binding.progressbar.visibility = View.VISIBLE
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+
+                binding.webview.visibility = View.VISIBLE
+                binding.progressbar.visibility = View.GONE
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+
+        if (binding.webview.canGoBack()){
+            binding.webview.goBack()
+        }else{
+            super.onBackPressed()
+        }
+
+    }
+}
